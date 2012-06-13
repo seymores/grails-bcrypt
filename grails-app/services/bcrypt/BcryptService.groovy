371db976b1e2b14363a72f3725521215e62260ff
Burt Beckwith <beckwithb@vmware.com>
2012-06-12 21:16:22 -0400

    changed calls of static methods from an unnecessary instance to a static call; also removed redundant 'def' from return types - use def or the real type, but not both

--- grails-app/services/bcrypt/BcryptService.groovy
+++ grails-app/services/bcrypt/BcryptService.groovy
@@ -2,20 +2,17 @@ package bcrypt
 
 import grails.plugin.bcrypt.BCrypt
 
-
 class BcryptService {
 
-    def bcrypt = new BCrypt()
-
-    def String hashPassword(String pass) {
-        return  bcrypt.hashpw(pass, BCrypt.gensalt())
-    }
-    
-    def String hashPassword(String pass, int genSaltRound) {
-        return  bcrypt.hashpw(pass, BCrypt.gensalt(genSaltRound))
+    String hashPassword(String pass) {
+        return BCrypt.hashpw(pass, BCrypt.gensalt())
     }
 
-    def boolean checkPassword(String pass, String hash) {
-        return bcrypt.checkpw(pass, hash)
+    String hashPassword(String pass, int genSaltRound) {
+        return BCrypt.hashpw(pass, BCrypt.gensalt(genSaltRound))
+    }
+
+    boolean checkPassword(String pass, String hash) {
+        return BCrypt.checkpw(pass, hash)
     }
 }
